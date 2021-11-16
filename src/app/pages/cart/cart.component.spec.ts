38b12d9310cb1b80e0f125ef272bacb13ccd68ab
Diego Almeida <dyhalmeida@gmail.com>
2021-11-16 09:50:24 -0300

    Arquivo de teste do cart.component criado.
    O ponto importante desse commit é a variável fixture.
    Pois é dela que será possível extrair coisas do cartComponent,
    como o serviço injetado e detectar mudanças com o método ngOnInit.

---
+++ src/app/pages/cart/cart.component.spec.ts
@@ -0,0 +1,9 @@
+import { ComponentFixture } from "@angular/core/testing";
+import { CartComponent } from "./cart.component";
+
+describe('cart.component', () => {
+
+  let cartComponent: CartComponent;
+  let fixture: ComponentFixture<CartComponent>;
+
+});
