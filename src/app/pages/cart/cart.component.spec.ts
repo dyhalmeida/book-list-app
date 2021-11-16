d0eab675e69f1db16894afc32c9aef9f6ad15f77
Diego Almeida <dyhalmeida@gmail.com>
2021-11-16 10:01:03 -0300

    Configuração do módulo de testes do cartComponent.
    Neste commit o ponto importante é a configuração do módulo
    do teste unitário com o TestBed.configureTestingModule.
    Nota-se que essa configuração é feita dentro da callback da função
    beforeEach. Isso por que o beforeEach é executado SEMPRE antes de cada
    teste unitários.
    
    O configureTestingModule é semelhante a um módulo do Angular, no qual devemos
    declarar o import de módulos que o teste irá utilizar, os componentes utilizados
    no testes, os providers que os componentes tem injetados em seus construtores e alguns
    schemas para evitar erros durante o processo de testes do Jasmine.

--- src/app/pages/cart/cart.component.spec.ts
+++ src/app/pages/cart/cart.component.spec.ts
@@ -1,9 +1,31 @@
-import { ComponentFixture } from "@angular/core/testing";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { BookService } from "src/app/services/book.service";
 import { CartComponent } from "./cart.component";
 
 describe('cart.component', () => {
 
   let cartComponent: CartComponent;
   let fixture: ComponentFixture<CartComponent>;
+
+  beforeEach(() => {
+
+    TestBed.configureTestingModule({
+
+      imports: [
+        HttpClientTestingModule
+      ],
+      declarations: [
+        CartComponent,
+      ],
+      providers: [
+        BookService,
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+
+    }).compileComponents();
+
+  });
 
 });
