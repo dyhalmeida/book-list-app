17efa0e9b78a50331de0f08aafd8e8a35833cf45
Diego Almeida <dyhalmeida@gmail.com>
2021-11-17 20:06:36 -0300

    Teste de método com retorno (getTotalPrice).
    O ponto importante desse commit é o teste de um método
    do component que possui retorno.

--- src/app/pages/cart/cart.component.spec.ts
+++ src/app/pages/cart/cart.component.spec.ts
@@ -1,6 +1,7 @@
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { Book } from "src/app/models/book.model";
 import { BookService } from "src/app/services/book.service";
 import { CartComponent } from "./cart.component";
 
@@ -39,6 +40,31 @@ describe('cart.component', () => {
   it('Should create', () => {
 
     expect(cartComponent).toBeTruthy();
+
+  });
+
+  it('getTotalPrice returns an amount', () => {
+
+    const listBook: Book[] = [
+      {
+        author: '',
+        name: '',
+        isbn: '',
+        amount: 2,
+        price: 10,
+      },
+      {
+        author: '',
+        name: '',
+        isbn: '',
+        amount: 3,
+        price: 10,
+      },
+    ];
+
+    const totalPrice = cartComponent.getTotalPrice(listBook);
+
+    expect(totalPrice).toBe(50);
 
   });
 
