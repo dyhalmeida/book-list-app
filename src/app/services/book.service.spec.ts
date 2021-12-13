d7ae2305de77893716399566e003d454a730d52c
Diego Almeida <dyhalmeida@gmail.com>
2021-12-13 06:28:14 -0300

    Adicionado teste de addBookToCart.
    O ponto importante desse teste é o uso de spyOn em getItem, setItem e
    mixin, para fazer uma chamada fake no qual são dependência de addBookToCart.

--- src/app/services/book.service.spec.ts
+++ src/app/services/book.service.spec.ts
@@ -2,10 +2,11 @@ import { HttpClientTestingModule, HttpTestingController } from "@angular/common/
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from "@angular/core";
 import { TestBed } from "@angular/core/testing";
 import { environment } from "src/environments/environment.prod";
+import Swal from "sweetalert2";
 import { Book } from "../models/book.model";
 import { BookService } from "./book.service";
 
-describe('book.service', () => {
+fdescribe('book.service', () => {
 
   let bookService: BookService;
   let httpMock: HttpTestingController;
@@ -113,6 +114,46 @@ describe('book.service', () => {
     const listBook = bookService.getBooksFromCart();
     expect(spy01).toHaveBeenCalled();
     expect(listBook.length !== 0).toBeTrue();
+
+  });
+
+  it('should be call addBookToCart and add a book successfully when the list does not exist in the localstorage', () =>{
+
+    const toast = {
+      fire: () => null,
+    } as any;
+
+    const book: Book = {
+      author: '',
+      name: '',
+      isbn: '',
+      amount: 2,
+      price: 10,
+    };
+
+    const storageMock = {};
+
+    const spy01 = spyOn(localStorage, 'getItem').and.callFake((key: string) => {
+      return storageMock[key] ? JSON.stringify(storageMock[key]) : null;
+    });
+
+    const spy02 = spyOn(localStorage, 'setItem').and.callFake((key: string, value: string) => {
+      return storageMock[key] = value;
+    });
+
+    const spy03 = spyOn(Swal, 'mixin').and.callFake(() => toast)
+
+    let listBook = bookService.getBooksFromCart();
+    expect(listBook.length === 0).toBeTrue();
+
+    bookService.addBookToCart(book);
+
+    listBook = bookService.getBooksFromCart();
+    expect(listBook.length !== 0).toBeTrue();
+
+    expect(spy01).toHaveBeenCalled();
+    expect(spy02).toHaveBeenCalled();
+    expect(spy03).toHaveBeenCalled();
 
   });
 
